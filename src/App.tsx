da317c6636fef953ed8503b33ea0544c8c604731
Paul DAO <dao.van.paul@gmail.com>
2020-06-05 16:40:47 +0200

    add : connection with edamam api and get request in console

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
 import './App.scss';
 
-function App() {
-  return (
+const App = () => {
+
+  const APP_ID = 'bfae6ace';
+  const APP_KEY = '890847a09f410c0adb3992a92b1222f7';
+
+  useEffect(() => {
+    getRecipes()
+  }, [])
+
+  const getRecipes = async () => {
+    const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
+    const data = await response.json()
+    console.log(data.hits);    
+  }
+
+  return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form className="search-form">
+        <input type="text" className="search-bar"/>
+        <button className="search-button">Search</button>
+      </form>
     </div>
-  );
+  )
 }
 
 export default App;
