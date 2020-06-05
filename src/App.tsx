2488005644190625cca3137fe829dae1ef4e3d45
Paul DAO <dao.van.paul@gmail.com>
2020-06-05 21:33:03 +0200

    feat : functionnal search form

--- src/App.tsx
+++ src/App.tsx
@@ -9,24 +9,39 @@ const App = () => {
   const APP_KEY = '890847a09f410c0adb3992a92b1222f7';
 
   // Need to specify the type of UseState
-  const [recipes, setRecipes] = useState<Hit[]>([])
+  const [recipes, setRecipes] = useState<Hit[]>([]);
+  const [search, setSearch] = useState<string>('');
+  const [query, setQuery] = useState('chicken');
 
+  // 
   useEffect(() => {
     getRecipes()
-  }, [])
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [query])
 
   const getRecipes = async () => {
-    const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
+    const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
     const data = await response.json();    
-    setRecipes(data.hits);    
+    setRecipes(data.hits);
+  }
+
+  const updateSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+  }
+
+  const getSearch = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setQuery(search);
   }
 
   return(
     <div className="App">
-      <form className="search-form">
-        <input type="text" className="search-bar"/>
+      <form onSubmit={getSearch} className="search-form" >
+        <input type="text" className="search-bar" value={search} onChange={updateSearch}/>
         <button className="search-button">Search</button>
       </form>
+      
+      <p>Recipes : {query}</p>
 
       {recipes.map((recipe) => (  
         <Recipe 
